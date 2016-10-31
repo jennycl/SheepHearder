0b7e21d8c009eea3d2f514aebaac32c2987b295f
Anna Chiu <awc316@nyu.edu>
2016-10-31 12:48:35 -0400

    updated sketch with comments etc

--- sketch.js
+++ sketch.js
@@ -1,51 +1,58 @@
 // global variables for images and time
-var sheeps = [ ]; // array of sheep objects
-
 var moveAwayTime = 5; // sheep moves away from dog when collision detected for 5 frames
-//var gameTime = 60; // max time to win game - start countdown
 
+// holds time left ot play game and time game is played
 var timeLeft;
 var timePassed = 0;
 
+// canvas dimensions
 var theCanvas;
 var canvasWidth = 1000;
 var canvasHeight = 600;
+
+// pen dimensions and positioning
 var penX = canvasWidth;
 var penY = canvasHeight/2;
+var penWidth = 70; 
+var penHeight = canvasHeight/2; 
+
+// start button positioning
 var startButtonX = canvasWidth * 0.90;
 var startButtonY = canvasHeight *0.85;
 
+// hold start screen sound and bahh sound
 var sound; 
 var sheepSound;
 
+// how many times the game has been played - for leveling
 var timesPlayed = 0; 
-
-//var gameState = 0;
 
 // start and end conditions
 var start = false;
 var end = false;
 var showEnd = false; // when to show end screen
 
-var dog;
-var mic;
-
-var penWidth = 70; 
-var penHeight = canvasHeight/2; 
-
+// characters, canvas, and mic
+var dog; // ones dog object
+var sheeps = [ ]; // array of sheep objects
 var SheepSprites;
 var DogSprites;
 var canvas;
+var mic;
 
+// background images depending on state
 var startscreen;
 var endscreen; 
 var backgroundImage;
 
+// keeps track of number of sheep left to herd
 var sheepLeft;
 
+//
 var count = 1;
 var frames = 0;
 
+// 'empty' object to figure out which dog image to use
 var currentDog = {
   xpos:0,
   ypos:0,
@@ -53,84 +60,66 @@ var currentDog = {
   h:0
 };
 
-//var orangeFont;
-
-
-//var currentDog = image (DogSprites,DogLeft1.xpos,DogLeft1.ypos,DogLeft1.w,DogLeft1.h,this.x,this.y,DogLeft1.w,DogLeft1.h);
-
-
+// preload images, sounds, and fonts
 function preload(){
+  // load images
   SheepSprites = loadImage("images/sheepSprite.png");
   DogSprites = loadImage("images/dogSprite.png");
   grass = loadImage("images/grass3.png");
-  
-  // load images
   startscreen = loadImage("images/start-screen.png");
   endscreen = loadImage("images/end-screen.png");
   backgroundImage = loadImage("images/background-with-pen-fixed.png");
   
-  sound = loadSound('trim2.mov');
-  sheepSound = loadSound('baa.mp3');
-  
-  //orangeFont = loadFont("/fonts/Rich_M_Font.ttf");
+  // load sounds
+  sound = loadSound("sounds/trim2.mov");
+  sheepSound = loadSound("sounds/baa.mp3");
 }
 
 function setup(){
-    theCanvas = createCanvas(canvasWidth,canvasHeight);
-    theCanvas.style('border','5px solid black');
-    theCanvas.style('display','block');
-    theCanvas.style('margin','auto');
+  // create and style the canvas
+  theCanvas = createCanvas(canvasWidth,canvasHeight);
+  theCanvas.style('border','5px solid black');
+  theCanvas.style('display','block');
+  theCanvas.style('margin','auto');
 
-    noiseDetail(24);
+  // set noise detail for Perlin noise
+  noiseDetail(24);
     
-    // get audio through mic
-    mic = new p5.AudioIn()
-    mic.start();
+  // get audio through mic
+  mic = new p5.AudioIn()
+  mic.start();
     
-
-    // create lots of sheep
-    for (var i = 0; i < 10; i++) {
-      sheeps.push( new Sheep(random(width-canvasWidth/5), random(height),randomSheepPic()) );
-    }
+  // create lots of sheep
+  for (var i = 0; i < 10; i++) {
+    sheeps.push( new Sheep(random(width-canvasWidth/5), random(height),randomSheepPic()) );
+  }
     
-    sheepLeft = sheeps.length;
-    // console.log("Sheeps left: " + sheepLeft);
+  // get the number of sheep in the array initially  
+  sheepLeft = sheeps.length;
     
-    // create the dog
-    dog = new Dog(0,0);
+  // create the dog
+  dog = new Dog(0,0);
 }
 
 function draw(){
-  //console.log("micLevel: " + mic.getLevel());
   imageMode(CORNER);
   image(grass,0,0,canvasWidth, canvasHeight);
   imageMode(CENTER);
   
- 
-  
   // startScreen
   if(start == false && end == false){
     startScreen();
-    
-    drawButton(mouseX,mouseY);
   }
   
-  // if(gameState == 1){
-  //   instructionScreen();
-  // }
-  // playScreen
-  //if(gameState == 1){
-  
+  // play game
   else if (start == true && end == false){ 
+    // background and time left display
     imageMode(CORNER);
     image(backgroundImage,0,0,canvasWidth,canvasHeight);
     textAlign(LEFT,TOP);
     fill(255);
     text("Time Left:" + ceil(timeLeft/60),20,20);
 
-    // draws pen 
-    Pen();
-    
     // draw all of our sheeps
     for (var i = 0; i < sheeps.length; i++) {
       sheeps[i].move();
@@ -139,14 +128,6 @@ function draw(){
       collisionDogToSheep(dog, sheeps[i]); // check for collision between dog and all sheep
     }
     
-    // remove 
-    // for (var i = 0; i < sheeps.length; i++) {
-    //   if(sheeps[i].penCollision){
-    //     sheeps.splice(i,1);
-    //   }
-    // }
-    //console.log("size of sheeps" + sheeps.length);
-    
     // check collision between all sheep
     for (var i = 0; i < (sheeps.length - 1); i++){
       for(var j = 1; j < sheeps.length; j++){
@@ -154,45 +135,50 @@ function draw(){
       }
     }
     
-    
+    // dog display and actions
     dog.move();
     dog.display();
     dog.penCollision();
     
-    
-    if (count <4){
+    // choose which image of the dog sprite to be shown
+    if (count < 4){
       count++
     }
     else{
-      count=1
+      count = 1
     }
+    
+    // reset frames every 60 frames
     frames++;
     if (frames > 60){
       frames = 0;
     }
     
+    // decrement time left
     timeLeft -= 1;
     
-    // check collision between sheep - randomly moving 
-    
+    // check collisions between sheep and pen - if sheep in pen, delete from array/game
     for (var j = 0; j < sheeps.length ;j++){
-      if (sheepToPenCollision(sheeps[j])){
+      if (sheeps[j].penCollision()){
         sheeps.splice(j, 1);
       }
     }
-    if (nextLevel() && timesPlayed < 2){
-          // create lots of sheep
+    
+    // if the player reaches next level (and they've only played the first level,  then refill the game with more sheep)
+    if (nextLevel() && timesPlayed < 2){ 
+      // create lots of sheep
       for (var i = 0; i < 20; i++) {
         sheeps.push( new Sheep(random(width-canvasWidth/5), random(height),randomSheepPic()) );
       }
-      
+      // check number of sheep left in array
       sheepLeft = sheeps.length;
-      // console.log("Sheeps left: " + sheepLeft);
     }
     
+    // constantly check if game over
     gameOver();
-    
   }
+  
+  // do game over events if game over
   else if (start == false && end == true){
     gameOver();
   }
@@ -200,6 +186,7 @@ function draw(){
 
 // Sheep constructor
 function Sheep(x, y, name){
+  // holds current sheep sprite image to be used
   var currentSheep = {
     xpos:0,
     ypos:0,
@@ -207,39 +194,26 @@ function Sheep(x, y, name){
     h:0
   }
   
+  // sheep position
   this.x = x;
   this.y = y;
-  this.name = name;
-  //this.state = 0;
-  this.framesInState = 0;
   
+  // get the name of the current sheep to be displayed
+  this.name = name;
   currentSheep = name;
 
+  // number of frames sheep should stay as a particular image
+  this.framesInState = 0;
+  
   // compute a perlin noise offiset
   this.noiseOffsetX = random(0,1000);
   this.noiseOffsetY = random(0,1000);
 
   // display the sheep
   this.display = function(){
-    
      imageMode(CENTER);
-      // currentDog = image (DogSprites,DogLeft1.xpos,DogLeft1.ypos,DogLeft1.w,DogLeft1.h,this.x,this.y,DogLeft1.w,DogLeft1.h);
-       //currentDog = DogLeft1;
-      // if(frames %10 ==0){
-      //   if(count == 1){
-      //     image (SheepSprites,SheepDown1.xpos,SheepDown1.ypos,SheepDown1.w,SheepDown1.h,this.x,this.y,SheepDown1.w,SheepDown1.h);
-      //   }
-      //   if(count == 2){
-      //     image (SheepSprites,SheepDown2.xpos,SheepDown2.ypos,SheepDown2.w,SheepDown1.h,this.x,this.y,SheepDown2.w,SheepDown2.h);
-      //   }
-      //   if(count == 3){
-      //     image (SheepSprites,SheepDown3.xpos,SheepDown3.ypos,SheepDown3.w,SheepDown3.h,this.x,this.y,SheepDown3.w,SheepDown3.h);
-      //   }
-      //   if(count == 4){
-      //     image (SheepSprites,SheepDown4.xpos,SheepDown4.ypos,SheepDown4.w,SheepDown4.h,this.x,this.y,SheepDown4.w,SheepDown4.h);
-      //   }
-      // }
       image (SheepSprites,currentSheep.xpos,currentSheep.ypos,currentSheep.w,currentSheep.h,this.x,this.y,currentSheep.w,currentSheep.h);
+      // once direction of sheep to face is decided - animate the image using mutiple images with slightly altered positioning
       if(this.name == SheepDown2){
         if(this.framesInState % 10==0){
           currentSheep = SheepDown2;
@@ -291,7 +265,6 @@ function Sheep(x, y, name){
           currentSheep = SheepDown3;
         }
       }
-    
     if(this.name == SheepDown4){
         if(this.framesInState % 10==0){
           currentSheep = SheepDown4;
@@ -310,44 +283,21 @@ function Sheep(x, y, name){
         }
       }
       
+      // increment frames and reset to 0 when 90 frames have passed
       this.framesInState +=1
       if(this.framesInState > 90){
         this.framesInState = 0;
       }
-    
-    // image (SheepSprites,this.name.xpos,this.name.ypos,this.name.w,this.name.h,this.x,this.y,this.name.w,this.name.h);
-    
-    // ellipseMode(CENTER);
-    // ellipse(this.x,this.y,50,50);
-    // if(xPos < xFox){
-    //   xFox -= foxSpeed;
-    //   fox_current = fLeft;
-    // }
-    // if(xPos > xFox){
-    //   xFox += foxSpeed;
-    //   fox_current = fRight;
-    // }
-    // if(yPos < yFox){
-    //   yFox -= foxSpeed;
-    //   fox_current = fUp;
-    // }
-    // if(yPos > yFox){
-    //   yFox += foxSpeed;
-    //   fox_current = fDown;
-    // }
   }
 
   // move this sheep
   this.move = function() {
-    
-    
     // movement of sheep in x and y direction
     var xMovement = map(noise(this.noiseOffsetX), 0, 1, -1.5, 1.5);
     this.x += xMovement;
     var yMovement = map(noise(this.noiseOffsetY), 0, 1, -1, 1);
     this.y += yMovement;
-
-    // console.log("HI" + (width-canvasWidth/5));
+    
     // sheep should bounce off the walls of the screen - want wraparound but not working
     if (this.x > (width-canvasWidth/5)+25 && (this.y > (height-canvasHeight/4)|| this.y < (canvasHeight/4))) {
       this.x -= 2;
@@ -366,30 +316,29 @@ function Sheep(x, y, name){
     this.noiseOffsetX += 0.01;
     this.noiseOffsetY += 0.01;
   }
-
-  // checks if this sheep goes into the pen area - returns true or false
-  this.penCollision = function(){
-    // if hits the pen, then goes into the pen (disappears)
-  }
   
-  // goal - when a sheep collides with other sheep collect info of where the sheep is coming from and tell
-  // them to move in other direction.
-  // this.afterCollision = function(x,y){
-  //   this.x += x;
-  //   this.y += y;
-  // }
+  // detect collision beterrn sheep and pen and play bahh sound when it occurs
+  this.penCollision = function() {
+    if (this.x > canvasWidth - canvasWidth/10 && (this.y < canvasHeight/2 + canvasHeight/4) && (this.y > canvasHeight/2 - canvasHeight/4)){
+      sheepSound.play();
+      return true;
+    } 
+    else{
+      return false;
+    }
+  }
 }
 
+// determines whether or not next level is reached
 function nextLevel(){
-  
   if (sheeps.length == 0 && timeLeft > 0 && timesPlayed < 2){
     timeLeft = 6000;
     timesPlayed++;
     return true;  
   }
-  
 }
 
+// choose which sheep to display randomly
 function randomSheepPic(){
   switch(Math.floor(Math.random() * (3 + 1))){
     case 0:
@@ -400,17 +349,6 @@ function randomSheepPic(){
       return SheepDown3;
     case 3:
       return SheepDown4;
-  }
-  
-}
-
-function sheepToPenCollision(sheep){
-  if (sheep.x > canvasWidth - canvasWidth/10 && (sheep.y < canvasHeight/2 + canvasHeight/4) && (sheep.y > canvasHeight/2 - canvasHeight/4)){
-    sheepSound.play();
-    return true;
-  } 
-  else{
-    return false;
   }
 }
 
@@ -424,30 +362,23 @@ function Dog(x, y){
     //ellipse(this.x, this.y, 30);
     if(this.direction == 1 && !(keyIsDown(65) || keyIsDown(LEFT_ARROW))){
       currentDog = DogLeft2;
-
     }
     if(this.direction == 2 && !(keyIsDown(68) || keyIsDown(RIGHT_ARROW))){
       currentDog = DogRight2;
-     
     }
-    
     if(this.direction == 3 && !(keyIsDown(87) || keyIsDown(UP_ARROW))){
       currentDog = DogUp2;
-     
     }
-    
     if(this.direction == 4 && !(keyIsDown(83) || keyIsDown(DOWN_ARROW))){
       currentDog = DogDown1;
     }
-
   }
 
-  // handles dog's collision with the pen - not working - later adapt to sheep
+  // handles dog's collision with the pen
   this.penCollision = function(){
     if (this.x >= (canvasWidth - penWidth/2) &&  this.y <= (canvasHeight - penHeight + penHeight/2) && this.y >= (canvasHeight - penHeight - penHeight/2)){ // if 
   	  this.x = canvasWidth - penWidth/2;
-  	}
-  	// ******* ISSUE *************// 
+    }
   	else if(this.x >= canvasWidth - penWidth/2 && this.y == canvasHeight/4 ){ // if on top edge
   	  this.y = canvasHeight/4;
   	}
@@ -456,14 +387,13 @@ function Dog(x, y){
   	}
   }
   
+  // holds which image direction dog is displayed in
   this.direction = 1;
 
-  // keyboard directed movement of dog 
+  // keyboard directed movement of dog - determines which image of the dog should be displayed
   this.move = function(){
     if (keyIsDown(65) || keyIsDown(LEFT_ARROW)){
       imageMode(CENTER);
-      // currentDog = image (DogSprites,DogLeft1.xpos,DogLeft1.ypos,DogLeft1.w,DogLeft1.h,this.x,this.y,DogLeft1.w,DogLeft1.h);
-       //currentDog = DogLeft1;
       if(frames %5 ==0){
         if(count == 1){
           currentDog = DogLeft1;
@@ -483,8 +413,6 @@ function Dog(x, y){
     }
     if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
       imageMode(CENTER);
-      // currentDog = image (DogSprites,DogRight1.xpos,DogRight1.ypos,DogRight1.w,DogRight1.h,this.x,this.y,DogRight1.w,DogRight1.h);
-      //currentDog = DogRight1;
       if(frames %5 ==0){
         if(count == 1){
           currentDog = DogRight1;
@@ -502,11 +430,8 @@ function Dog(x, y){
       this.x += 5;
       this.direction = 2;
     }
-    
     if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
       imageMode(CENTER);
-      // currentDog = image (DogSprites,DogUp1.xpos,DogUp1.ypos,DogUp1.w,DogUp1.h,this.x,this.y,DogUp1.w,DogUp1.h);
-      //currentDog = DogUp1;
       if(frames %5 ==0){
         if(count == 1){
           currentDog = DogUp1;
@@ -524,7 +449,6 @@ function Dog(x, y){
       this.y -= 5;
       this.direction = 3;
     }
-    
     if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
       imageMode(CENTER);
       // currentDog = DogDown2;
@@ -546,7 +470,7 @@ function Dog(x, y){
       this.direction = 4;
     }
  
-     // prevent dog from moving beyond boundries
+    // prevent dog from moving beyond boundries
     if (this.x > canvasWidth - (canvasWidth/10)){  
       this.x = canvasWidth - (canvasWidth/10); 
     }
@@ -554,18 +478,9 @@ function Dog(x, y){
     if (this.x <  0){ this.x = 0; }
     if (this.y > height){ this.y = height; }
     
+    // display the current dog image
     image (DogSprites,currentDog.xpos,currentDog.ypos,currentDog.w,currentDog.h,this.x,this.y,currentDog.w,currentDog.h);
   }
-  
-  
-}
-
-// draw a pen at the right side of the screen
-function Pen(){
-  fill(188,143,143);
-  rectMode(CENTER);
-  //rect(penX,penY,canvasWidth/5,canvasHeight/2);
-  //console.log(canvasWidth/5);
 }
 
 // detects collision between dog and the sheep
@@ -597,7 +512,7 @@ function collisionSheepToSheep(sheepA, sheepB){
   }
 }
 
-// sheep move away from dog - check if changes work
+// sheep move away from dog but not beyond borders 
 function moveAway(dog, sheep){
   if(dog.x < sheep.x){
     sheep.x += 1;
@@ -650,6 +565,7 @@ function heardBark(dog, sheep){
   // change display based on if it is in radius of dog 
   micLevel = mic.getLevel();
   
+  // holds the radius and whether player is barking
   var radius = 0;
   var isBarking = false;
   
@@ -680,50 +596,27 @@ function heardBark(dog, sheep){
       moveAway(dog, sheep);
     }
   }
-  // else if(micLevel > 0.8){
-  //   radius = 175;
-  //   isBarking = true;
-  //   dogGlow(dog,radius,isBarking);
-  //   if(dist(dog.x, dog.y, sheep.x, sheep.y) < radius){
-  //     moveAway(dog, sheep);
-  //   }
-  // }
   else{
     isBarking = false;
     radius = 0;
     dogGlow(dog,radius,isBarking);
   }
-  // else{
-  //   radius = 200;
-  //   isBarking = true;
-  //   dogGlow(dog,radius,isBarking);
-  //   if(dist(dog.x, dog.y, sheep.x, sheep.y) < radius){
-  //     moveAway(dog, sheep);
-  //   }
-  // }
 }
 
 var fillOpacity = 0;
 var ellipseRadius = 0;
-  
+
+// draws a glowing yellow globe/ball around dog when barking is picked up
 function dogGlow(dog, radius, isBarking){
-  // var r =
-  // var g = 
-  // var b =
-  
   if(isBarking){
-    
     if(ellipseRadius < radius){ // constrain the radius of the glow ball
       fill(255,255,fillOpacity); // get shades of yellow by changing the blue color values and setting red and green to 255
       stroke(255,255,fillOpacity);
       ellipse(dog.x,dog.y,ellipseRadius,ellipseRadius);
       
       fillOpacity += 10;
-    
       ellipseRadius += 5;
-      //console.log("ellipse glow: " +  ellipseRadius);
     }
-    
     else{
         fillOpacity = 0;
         ellipseRadius = 0;
@@ -735,6 +628,7 @@ function dogGlow(dog, radius, isBarking){
   }
 }
 
+// check if player has won
 function doYouWin(){
   if (sheeps.length === 0 && timeLeft > 0){
     return true;
@@ -744,11 +638,10 @@ function doYouWin(){
   }
 }
 
+// game over events
 function gameOver(){
   // increment one second per frame (assume 60 frames per second)
   timePassed += 1;
-  //console.log(timePassed);
-  // if(gameTime == 7200){
   if(timePassed == 6000){
     // set start to false and end to true so draw no longer runs the game
     start = false;
@@ -756,9 +649,9 @@ function gameOver(){
     showEnd = true; // true when end image to be shown
   }
   
+  // show end screen
   if(showEnd || sheeps.length == 0){
-
-   imageMode(CORNER);
+    imageMode(CORNER);
     // reset background image according to results of game
     image(endscreen,0,0,canvasWidth,canvasHeight);
     if(doYouWin()){
@@ -780,48 +673,22 @@ function gameOver(){
   }
 }   
     
-
 //This function displays start screen
 function startScreen(){
-  
- 
+  // play sound if not playing
   if (sound.isPlaying() === false){
     sound.play();
   }
-   // start at 120 seconds
-  // gameTime = 7200;
+  timeLeft = 6000;
   
-  timeLeft = 6000; // used for testing
   imageMode(CORNER);
   image(startscreen,0,0,canvasWidth,canvasHeight);
+  
   drawButton(mouseX,mouseY);
   
   fill(255, 255, 151);
-  //textFont(orangeFont);
   textSize(80);
   text("Sheep Heard-er!", canvasWidth/2, canvasHeight/10);
-  
-  /*
-  background(0);
-  fill(255);
-  
-  //textFont(scoobyFont);
-  textSize(20);
-  rectMode(CENTER);
-  fill(255,255,153);
-  stroke(255,204,102);
-  strokeWeight(5);
-  textSize(23);
-    
-  //start button
-  rect(startButtonX, startButtonY, 150, 50);
-  textSize(40);
-  fill(0);
-  textAlign(CENTER,CENTER);
-  text("START",startButtonX,startButtonY);
-  */
-  
- 
 }
 
 //This function checks if start button is hovered over or pressed
@@ -868,10 +735,7 @@ function drawButton(testX, testY){
   }
 }
 
-// function instructionScreen(){
-//   background(0);
-// }
-
+// objects to hold positions of the dog and sheep sprite within a larger image to determine which image to use
 var SheepUp1 = {
   xpos: 50,
   ypos: 40,
